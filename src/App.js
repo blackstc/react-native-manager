2a182f79c8294b23c4a6e0d96ce7395a7dcf136c
Charlie Blackstock <charlieblackstock@gmail.com>
2017-01-30 17:12:32 -0700

    add redux and login form

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import firebase from 'firebase';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
+
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyD3pq8PcUQiKVOLPAwhUxD25xotkO_qiTk',
+      authDomain: 'manager-92a59.firebaseapp.com',
+      databaseURL: 'https://manager-92a59.firebaseio.com',
+      storageBucket: 'manager-92a59.appspot.com',
+      messagingSenderId: '382726902224'
+    };
+
+    firebase.initializeApp(config);
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(reducers, {}, applyMiddleware(thunk))}>
+        <LoginForm />
+      </Provider>
+    )
+  }
+}
+
+export default App;
